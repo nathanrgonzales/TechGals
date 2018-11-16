3d0317865bd127b2a6a79d932192dc9fa29b1986
Nathan Gonzales <nathanrgonzales@hotmail.com>
2018-11-15 22:33:01 -0200

    Adiçao do projeto sem integraçao de nenhuma;

---
+++ Assets/sceMenuPrincipal/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+	public void DisableBoolInanimator (Animator anim)
+	{
+		anim.SetBool("isDisplayed", false);
+	}
+
+	public void EnableBoolInanimator (Animator anim)
+	{
+		anim.SetBool("isDisplayed", true);
+	}
+	
+	public void NavigateTo(int scene)
+	{
+		Application.LoadLevel(scene);
+	}
+
+	public void LoadScene(string nomeCena)
+	{
+		SceneManager.LoadScene(nomeCena);
+	}
+
+}
---
+++ Assets/sceSplashInicial/scrSplashFade.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class scrSplashFade : MonoBehaviour {
+	
+	public Image SplashImage;
+	public string LoadLevel;
+	
+	IEnumerator Start()
+	{
+		SplashImage.canvasRenderer.SetAlpha(0.0f);
+		FadeIn();
+		yield return new WaitForSeconds(3.5f);
+		FadeOut();
+		yield return new WaitForSeconds(2.5f);
+		if (LoadLevel != "") 
+			{SceneManager.LoadScene(LoadLevel);}
+	}
+	
+	void FadeIn()
+	{
+		SplashImage.CrossFadeAlpha(1.0f, 2.5f, false);
+	}
+
+	void FadeOut()
+	{
+		SplashImage.CrossFadeAlpha(0.0f, 2.5f, false);
+	}
+	
+}
